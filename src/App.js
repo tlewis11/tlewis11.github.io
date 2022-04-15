f2d2d29cff0e035a25b292a0f0fe21bf24ac3148
tlewis11 <tlewis11@github.com>
2022-04-15 01:10:47 -0500

    add projects list

--- src/App.js
+++ src/App.js
@@ -19,10 +19,13 @@ function App() {
       Tim Lewis<br/>
       Devops Automation Engineer<br/>
       <p>
-        Solving real world problems with technology and programming. <br/>
+        I solve real world problems with technology and programming. <br/>
 
         I love learning new technologies and teaching others.
+        
       </p>
+
+      My Projects: <br/><br/>
       <a href="use_python_to_anlayze_chess_games_with_stockfish_chess_engine.html">Running Stockfish with Python in Docker</a>
     </div>
 
