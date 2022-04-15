af8834e8180206af511d51409f718e898a41788a
tlewis11 <tlewis11@github.com>
2022-04-15 01:04:13 -0500

    add src and public folders

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+
+
+
+
+
+
+
+
+
+
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Tim Lewis<br/>
+      Devops Automation Engineer<br/>
+      <p>
+        Solving real world problems with technology and programming. <br/>
+
+        I love learning new technologies and teaching others.
+      </p>
+      <a href="use_python_to_anlayze_chess_games_with_stockfish_chess_engine.html">Running Stockfish with Python in Docker</a>
     </div>
+
   );
 }
 
